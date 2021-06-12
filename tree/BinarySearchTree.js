1a91590f8f0df170895275b9c65ee1436dba91f8
thkim <thkim2017@gmail.com>
2021-06-12 20:10:50 +0900

    Add insert method at Binary Search Tree

--- tree/BinarySearchTree.js
+++ tree/BinarySearchTree.js
@@ -10,6 +10,34 @@ class BinarySearchTree {
   constructor() {
     this.root = null;
   }
+
+  insert(value) {
+    var newNode = new Node(value);
+    if(this.root === null) {
+      this.root = newNode;
+      return this;
+    } else {
+      var current = this.root;
+      while(true) {
+        if(value === current.value) return undefined;
+        if(value < current.value) {
+          if(current.left === null) {
+            current.left = newNode;
+            return this;
+          } else {
+            current = current.left;
+          }
+        } else if(value > current.value) {
+          if(current.right === null) {
+            current.right = newNode;
+            return this;
+          } else {
+            current = current.right;
+          }
+        }
+      }
+    }
+  }
 }
 
 var tree = new BinarySearchTree();
