b2d195fc120a4b93473d814850133368cd45ed48
thkim <thkim2017@gmail.com>
2021-06-12 10:52:32 +0900

    Add insert method in Singly Linked List

--- singly-linked-list/Singly_Linked_List.js
+++ singly-linked-list/Singly_Linked_List.js
@@ -94,6 +94,22 @@ class SinglyLinkedList {
     }
     return false;
   }
+
+  insert(index, val) {
+    if(index < 0 || index > this.length) return false;
+    if(index === this.length) return this.push(val);
+    if(index === 0) return this.unShift(val);
+    
+    var newNode = new Node(val);
+    var prev = this.get(index - 1);
+    var temp = prev.next;
+    prev.next = newNode;
+    newNode.next = temp;
+    
+    this.length++;
+    
+    return true;
+    }
 }
 
 var list = new SinglyLinkedList();
