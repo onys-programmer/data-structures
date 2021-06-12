181b0c487790984c7cdbe01489a887cc5d32f772
thkim <thkim2017@gmail.com>
2021-06-12 15:59:27 +0900

    Add get method at Doubly Linked List

--- doubly-linked-list/Doubly-Linked-List.js
+++ doubly-linked-list/Doubly-Linked-List.js
@@ -67,4 +67,48 @@ class DoublyLinkedList {
 
     return oldHead;
   }
+
+  unshift(val) {
+    var newNode = new Node(val);
+    if(this.length === 0) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      this.head.prev = newNode;
+      newNode.next = this.head;
+      this.head = newNode;
+    }
+
+    this.length++;
+
+    return this;
+  }
+
+  get(index) {
+    if(index < 0 || index >= this.length) return null;
+
+    var count, current;
+
+    if(index <= this.length / 2) { // working from start
+      count = 0;
+      current = this.head;
+
+      while(count !== index) {
+        current = current.next;
+        count++;
+      }
+
+      return current;
+    } else { // working from end
+      count = this.length - 1;
+      current = this.tail;
+
+      while(count !== index) {
+        current = current.prev;
+        count--;
+      }
+
+      return current;
+    }
+  }
 }
