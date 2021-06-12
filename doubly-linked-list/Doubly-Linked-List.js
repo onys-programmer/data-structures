87d3cc8800b9594b6282520f00202d73e1ba2bbe
thkim <thkim2017@gmail.com>
2021-06-12 14:14:25 +0900

    Add pop method at Doubly Linked List

--- doubly-linked-list/Doubly-Linked-List.js
+++ doubly-linked-list/Doubly-Linked-List.js
@@ -29,4 +29,26 @@ class DoublyLinkedList {
     
     return this;
   }
+
+  pop() {
+    if(!this.head) return undefined;
+    
+    var poppedNode = this.tail;
+
+    if(this.length === 1) {
+      this.head = null;
+      this.tail = null;
+    } else {
+      this.tail = poppedNode.prev;
+      this.tail.next = null;
+      poppedNode.prev = null;
+    }
+
+    this.length--;
+
+    return poppedNode;
+  }
+
+
+
 }
