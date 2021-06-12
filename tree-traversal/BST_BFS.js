3c4079b7e5a770f0b7dfef0b7615addf0c58ec16
thkim <thkim2017@gmail.com>
2021-06-12 20:52:45 +0900

    Add BFS method at BST_BFS

---
+++ tree-traversal/BST_BFS.js
@@ -0,0 +1,89 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+class BinarySearchTree {
+  constructor() {
+    this.root = null;
+  }
+
+  insert(value) {
+    var newNode = new Node(value);
+    if(this.root === null) {
+      this.root = newNode;
+      return this;
+    } else {
+      var current = this.root;
+      while(true) {
+        if(value === current.value) return undefined;
+        if(value < current.value) {
+          if(current.left === null) {
+            current.left = newNode;
+            return this;
+          } else {
+            current = current.left;
+          }
+        } else if(value > current.value) {
+          if(current.right === null) {
+            current.right = newNode;
+            return this;
+          } else {
+            current = current.right;
+          }
+        }
+      }
+    }
+  }
+
+  find(value) {
+    if(this.root === null) return false;
+    var current = this.root;
+    var found = false;
+    while(current && !found) {
+      if(value < current.value) {
+        current = current.left;
+      } else if(value > current.value) {
+        current = current.right;
+      } else {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  findNode(value) {
+    if(this.root === null) return false;
+    var current = this.root;
+    var found = false;
+    while(current && !found) {
+      if(value < current.value) {
+        current = current.left;
+      } else if(value > current.value) {
+        current = current.right;
+      } else {
+        found = true;
+      }
+    }
+    if(!found) return undefined;
+    return current;
+  }
+
+  BFS() {
+    var node = this.root;
+    var data = [];
+    var queue = [];
+
+    queue.push(node);
+    while(queue.length) {
+      node = queue.shift();
+      data.push(node.value);
+      if(node.left) quque.push(node.left);
+      if(node.right) queue.push(node.right);
+    }
+    return data;
+  }
+}
