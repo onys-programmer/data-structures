a7f72decef70183c9a0737aa4044181b42a24657
thkim <thkim2017@gmail.com>
2021-06-12 10:32:45 +0900

    Add set method in Singly Linked List

--- singly-linked-list/Singly_Linked_List.js
+++ singly-linked-list/Singly_Linked_List.js
@@ -86,6 +86,14 @@ class SinglyLinkedList {
     return current;
   }
 
+  set(index, val) {
+    var foundNode = this.get(index);
+    if(foundNode) {
+      foundNode.val = val;
+      return true;
+    }
+    return false;
+  }
 }
 
 var list = new SinglyLinkedList();
