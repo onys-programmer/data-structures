9436e692b56d3fdd29fce156f2f30d9078a736ee
thkim <thkim2017@gmail.com>
2021-06-12 21:10:12 +0900

    Add DFSPostOrder method at BST_TreeTraversal

--- tree-traversal/BST_TreeTraversal.js
+++ tree-traversal/BST_TreeTraversal.js
@@ -97,4 +97,15 @@ class BinarySearchTree {
     traverse(this.root);
     return data;
   }
+
+  DFSPostOrder() {
+    var data = [];
+    function traverse(node) {
+      if(node.left) traverse(node.left);
+      if(node.right) traverse(node.right);
+      data.push(node.value);
+    }
+    traverse(this.root);
+    return data;
+  }
 }
