adfb9295ec49c792f8ed06570722244e4c7eb8ca
thkim <thkim2017@gmail.com>
2021-06-12 10:19:02 +0900

    Add unShift in Singly Linked List

--- singly-linked-list/Singly_Linked_List.js
+++ singly-linked-list/Singly_Linked_List.js
@@ -62,6 +62,19 @@ class SinglyLinkedList {
     return currentHead;
   }
 
+  unShift(val) {
+    var newNode = new Node(val);
+    if(!this.head) {
+      this.head = newNode;
+      this.tail = this.head;
+    } else {
+      newNode.next = this.head;
+      this.head = newNode;
+    }
+    this.length++;
+    return this;
+  }
+
 }
 
 var list = new SinglyLinkedList();
