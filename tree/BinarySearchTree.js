92986bfefe9380b2d9416b842f330e257de8259c
thkim <thkim2017@gmail.com>
2021-06-12 20:23:34 +0900

    Add find and findNode method at Binary Search Tree

--- tree/BinarySearchTree.js
+++ tree/BinarySearchTree.js
@@ -38,6 +38,39 @@ class BinarySearchTree {
       }
     }
   }
+
+  find(value) {
+    if(this.root === null) return false;
+    var current = this.root;
+    var found = false;
+    while(current && !found) {
+      if(value < current.value) {
+        current = current.left;
+      } else if(value > current.value) {
+        current = current.right;
+      } else {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  findNode(value) {
+    if(this.root === null) return false;
+    var current = this.root;
+    var found = false;
+    while(current && !found) {
+      if(value < current.value) {
+        current = current.left;
+      } else if(value > current.value) {
+        current = current.right;
+      } else {
+        found = true;
+      }
+    }
+    if(!found) return undefined;
+    return current;
+  }
 }
 
 var tree = new BinarySearchTree();
