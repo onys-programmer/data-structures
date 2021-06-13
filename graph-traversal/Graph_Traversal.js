82528fd112379e91516a2d816e56d4ccea1ff626
thkim <thkim2017@gmail.com>
2021-06-13 10:09:21 +0900

    Change the name of the file Fraph_DFS to Graph traversal Add breadthFirst method

--- graph-traversal/Graph_Traversal.js
+++ graph-traversal/Graph_Traversal.js
@@ -67,4 +67,24 @@ class Graph {
       });
     }
   }
+
+  breadthFirst(start) {
+    const queue = [start];
+    const result = [];
+    const visited = {};
+    let currentVertex;
+
+    visited[start] = true;
+    while(queue.length) {
+      currentVertex = queue.shift();
+      result.push(currentVertex);
+
+      this.adjacencyList[currentVertex].forEach(neighbor => {
+        if(!visited[neighbor]) {
+          visited[neighbor] = true;
+          queue.push(neighbor);
+        }
+      })
+    }
+  }
 }
